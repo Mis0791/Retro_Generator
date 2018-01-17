5576919f3b2e7efd409ee4b2910eb22d24665f6e
Michelle Sturgill <isabelsturgill@comcast.net>
2018-01-16 22:53:00 -0800

    Retro

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  blocks = [1,3,5,5,6,10,9,12,15,4];
+  colors = this.getRandomColor;
+  getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+  
 }
